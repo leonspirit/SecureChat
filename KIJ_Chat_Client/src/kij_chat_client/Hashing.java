3da0111de8b0d70b0f0f70ec8db39d8e43de043b
apasajaboleh1 <apasajaboleh2015@gmail.com>
2016-04-23 15:51:11 +0700

    hashing

---
+++ KIJ_Chat_Client/src/kij_chat_client/Hashing.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kij_chat_client;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author freddy
+ */
+public class Hashing {
+    public static String bytesToHex(byte[] b) {
+      char hexDigit[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                         '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+      StringBuffer buf = new StringBuffer();
+      for (int j=0; j<b.length; j++) {
+         buf.append(hexDigit[(b[j] >> 4) & 0x0f]);
+         buf.append(hexDigit[b[j] & 0x0f]);
+      }
+      return buf.toString();
+   }
+    public static String getshahasing(String input)
+    {
+         String hasil="";
+        try {
+            
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            md.update(input.getBytes());
+            byte[] output = md.digest();
+            hasil=bytesToHex(output);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return hasil;
+    }
+}
